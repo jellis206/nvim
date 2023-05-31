583bafe8c5c78bc6b0d3e5414309e2e426faed21
J. Ellis <jgellis63@gmail.com>
2023-05-31 11:57:47 -0600

    actually adds easy-pick

---
+++ lua/plugins/easy-pick.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "axkirillov/easypick.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      local base_branch = "develop"
+      local easypick = require("easypick")
+      easypick.setup({
+        pickers = {
+          {
+            name = "changed_files",
+            command = "git diff --name-only $(git merge-base HEAD " .. base_branch .. " )",
+            previewer = easypick.previewers.branch_diff({ base_branch = base_branch }),
+          },
+
+          -- list files that have conflicts with diffs in preview
+          {
+            name = "conflicts",
+            command = "git diff --name-only --diff-filter=U --relative",
+            previewer = easypick.previewers.file_diff(),
+          },
+        },
+      })
+    end,
+  },
+}
