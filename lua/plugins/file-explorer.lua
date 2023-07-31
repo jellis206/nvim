707bb592fbf7d99ae2ea7f8ef67157ce20fc1678
J. Ellis <jgellis63@gmail.com>
2023-07-31 11:42:12 -0600

    updated neotree

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -4,19 +4,19 @@ return {
     opts = {
       filesystem = {
         bind_to_cwd = false,
-        follow_current_file = true,
+        follow_current_file = { enabled = true },
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
           never_show = {
-            ".DS_Store"
+            ".DS_Store",
           },
           always_show = { -- remains visible even if other settings would normally hide it
             ".gitignored",
-            "node_modules"
-          }
-        }
-      }
-    }
-  }
+            "node_modules",
+          },
+        },
+      },
+    },
+  },
 }
