64d11126b0e39c99ed13182b2c38edb17b804042
Jay Ellis <jgellis206@gmail.com>
2023-10-25 18:41:25 -0600

    added lsp file operations

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,32 +3,59 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(client, buffer)
-          if client.name == "tsserver" then
-            vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", {
-              buffer = buffer,
-              desc = "Organize Imports",
-            })
-            vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", {
-              desc = "Rename File",
-              buffer = buffer,
-            })
-            vim.keymap.set("n", "<leader>cu", "<cmd>TypescriptRemoveUnused<CR>", {
-              desc = "Remove Unused Variables",
-              buffer = buffer,
-            })
-          end
-        end)
-      end,
-    },
     opts = {
-      autoformat = true,
+      -- make sure mason installs the server
       servers = {
+        ---@type lspconfig.options.tsserver
         tsserver = {
           -- root_dir = util.root_pattern(".git", "nx.json", "package.json", "tsconfig.json", "jsconfig.json"),
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>cu",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.removeUnused.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Remove Unused Imports",
+            },
+          },
+          settings = {
+            typescript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            javascript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
         },
         angularls = {
           -- root_dir = util.root_pattern(".git", "nx.json", "angular.json", "tsconfig.json"),
--- lua/plugins/refactor.lua
+++ lua/plugins/refactor.lua
@@ -5,5 +5,15 @@ return {
       require("refactoring").setup({})
       require("telescope").load_extension("refactoring")
     end,
-  }
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim",
+    },
+    config = function()
+      require("lsp-file-operations").setup({})
+    end,
+  },
 }
