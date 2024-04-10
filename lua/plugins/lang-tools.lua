8ec112db662a4343eaed13170e90d2b5fd138637
Jay Ellis <jgellis206@gmail.com>
2024-04-10 12:50:45 -0600

    updated nvim setup

--- lua/plugins/lang-tools.lua
+++ lua/plugins/lang-tools.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       ensure_installed = {
         "angular-language-server",
+        "typescript-language-server",
         "clangd",
         "clang-format",
         "cpplint",
