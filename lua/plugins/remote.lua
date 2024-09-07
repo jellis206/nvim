25a28c21b4e48a2c2f257722af8a292ab6dccc36
Jay Ellis <jgellis206+git@gmail.com>
2024-09-06 21:22:18 -0600

    make sure supermaven is not on at startup

---
+++ lua/plugins/remote.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "chipsenkbeil/distant.nvim",
+    branch = "v0.3",
+    config = function()
+      require("distant"):setup()
+    end,
+  },
+}
--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,6 +1,7 @@
 return {
   {
     "supermaven-inc/supermaven-nvim",
+    cmd = { "SupermavenStart" },
     opts = function()
       vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", { fg = "#6CC644" })
     end,
