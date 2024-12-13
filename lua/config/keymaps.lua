b25f3002f3be82a2d5250b3a58a07c6704892051
Jay Ellis <jgellis206+git@gmail.com>
2024-12-13 00:23:38 -0700

    added avante

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,105 +1,109 @@
-vim.api.nvim_set_keymap("n", "<S-Enter>", "<C-i>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<S-Enter>", "<C-i>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<S-Enter>", "<C-i>", { noremap = true, silent = true })
+-- Basic keymaps
+local function map(mode, lhs, rhs, opts)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, opts or { noremap = true, silent = true })
+end
+
+-- Shift+Enter mappings
+map("n", "<S-Enter>", "<C-i>")
+map("i", "<S-Enter>", "<C-i>")
+map("v", "<S-Enter>", "<C-i>")
 
 local wk = require("which-key")
 
+-- Normal mode mappings
 wk.add({
+  -- Telescope and search
   { "<leader>'", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
   { "<leader>/", LazyVim.pick("live_grep", { root = true }), desc = "Grep" },
   { "<leader><space>", LazyVim.pick("files", { root = true }), desc = "Find Files" },
-  { "<leader>A", "<cmd>Grapple reset<cr>", desc = "Clear All Grapple Tags" },
-  { "<leader>a", "<cmd>Grapple toggle<cr>", desc = "Grapple Toggle" },
-  { "<leader>bh", "<cmd>BufferLineCloseLeft<cr>", desc = "BufferLineCloseLeft" },
-  { "<leader>bl", "<cmd>BufferLineCloseRight<cr>", desc = "BufferLineCloseRight" },
-  { "<leader>cp", "<cmd> let @+ = expand('%:p') <cr>", desc = "Copy Current File Path" },
-  { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" },
-  { "<leader>j", "<cmd>Grapple cycle forward<cr>", desc = "Grapple Cycle Forward" },
-  { "<leader>k", "<cmd>Grapple cycle backward<cr>", desc = "Grapple cycle backward" },
-  { "<leader>p", group = "popups" },
-  { "<leader>pg", "<cmd>Grapple open_tags<cr>", desc = "Grapple Popup Tags" },
-  { "<leader>pm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
   { "<leader>s", group = "search" },
   { "<leader>sW", LazyVim.pick("grep_string", { word_match = "-w", root = false }), desc = "Word (cwd)" },
   { "<leader>sw", LazyVim.pick("grep_string", { word_match = "-w", root = true }), desc = "Word (root dir)" },
-  { "<leader>ug", "<cmd>GitBlameToggle<cr>", desc = "Toggle Git Blame" },
+
+  -- Buffer management
+  { "<leader>bh", "<cmd>BufferLineCloseLeft<cr>", desc = "Close Left Buffers" },
+  { "<leader>bl", "<cmd>BufferLineCloseRight<cr>", desc = "Close Right Buffers" },
+
+  -- Grapple
+  { "<leader>a", group = "Grapple" },
+  { "<leader>aA", "<cmd>Grapple reset<cr>", desc = "Clear All Grapple Tags" },
+  { "<leader>aa", "<cmd>Grapple toggle<cr>", desc = "Grapple Toggle" },
+  { "<leader>j", "<cmd>Grapple cycle forward<cr>", desc = "Grapple Cycle Forward" },
+  { "<leader>k", "<cmd>Grapple cycle backward<cr>", desc = "Grapple cycle backward" },
+  { "<leader>pg", "<cmd>Grapple open_tags<cr>", desc = "Grapple Popup Tags" },
+
+  -- Utility commands
+  { "<leader>A", group = "avante" },
+  { "<leader>cp", "<cmd>let @+ = expand('%:p')<cr>", desc = "Copy Current File Path" },
+  { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" },
+  { "<leader>p", group = "popups" },
+  { "<leader>pm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
   { "<leader>ut", "<cmd>Telescope undo<cr>", desc = "Undotree" },
 })
 
+-- Visual mode mappings
 wk.add({
-  {
-    mode = { "v" },
-    { "<leader>s", group = "Search" },
-    { "<leader>sW", LazyVim.pick("grep_string", { root = false }), desc = "Selection (cwd)" },
-    { "<leader>sw", LazyVim.pick("grep_string", { root = true }), desc = "Selection (root dir)" },
-  },
+  mode = { "v" },
+  { "<leader>s", group = "Search" },
+  { "<leader>sW", LazyVim.pick("grep_string", { root = false }), desc = "Selection (cwd)" },
+  { "<leader>sw", LazyVim.pick("grep_string", { root = true }), desc = "Selection (root dir)" },
 })
 
+-- Insert mode mappings
 wk.add({
-  {
-    mode = { "i" },
-    { "<C-$>", "<c-o>$", desc = "Move to end of line" },
-    { "<C-0>", "<c-o>0", desc = "Move to beginning of line" },
-    { "jj", "<esc>", desc = "Escape Insert Mode" },
-    { "kk", "<esc>", desc = "Escape Insert Mode" },
-  },
+  mode = { "i" },
+  { "<C-$>", "<c-o>$", desc = "Move to end of line" },
+  { "<C-0>", "<c-o>0", desc = "Move to beginning of line" },
+  { "jj", "<esc>", desc = "Escape Insert Mode" },
+  { "kk", "<esc>", desc = "Escape Insert Mode" },
 })
 
+-- Toggle configurations
+local function create_toggle(name, get_fn, set_fn, key)
+  Snacks.toggle
+    .new({
+      name = name,
+      get = get_fn,
+      set = set_fn,
+    })
+    :map(key)
+end
+
+-- Auto-complete toggle
 local auto_cmp_enabled = true
-Snacks.toggle
-  .new({
-    name = "Auto Complete",
-    get = function()
-      return auto_cmp_enabled
-    end,
-    set = function(state)
-      auto_cmp_enabled = state
-      vim.cmd("ToggleAutoComplete")
-    end,
-  })
-  :map("<leader>ua")
+create_toggle("Auto Complete", function()
+  return auto_cmp_enabled
+end, function(state)
+  auto_cmp_enabled = state
+  vim.cmd("ToggleAutoComplete")
+end, "<leader>ua")
 
-Snacks.toggle
-  .new({
-    name = "Copilot",
-    get = function()
-      return not require("copilot.client").is_disabled()
-    end,
-    set = function(state)
-      local copilot = require("copilot.command")
-      if state then
-        copilot.enable()
-      else
-        copilot.disable()
-      end
-    end,
-  })
-  :map("<leader>uc")
+-- Copilot toggle
+create_toggle("Copilot", function()
+  return not require("copilot.client").is_disabled()
+end, function(state)
+  local copilot = require("copilot.command")
+  if state then
+    copilot.enable()
+  else
+    copilot.disable()
+  end
+end, "<leader>uc")
 
+-- Git blame toggle
 local gb_enabled = true
-Snacks.toggle
-  .new({
-    name = "Git Blame",
-    get = function()
-      return gb_enabled
-    end,
-    set = function(state)
-      gb_enabled = state
-      vim.cmd("GitBlameToggle")
-    end,
-  })
-  :map("<leader>ug")
+create_toggle("Git Blame", function()
+  return gb_enabled
+end, function(state)
+  gb_enabled = state
+  vim.cmd("GitBlameToggle")
+end, "<leader>uB")
 
+-- Mouse toggle
 local mouse_enabled = true
-Snacks.toggle
-  .new({
-    name = "Mouse",
-    get = function()
-      return mouse_enabled
-    end,
-    set = function(state)
-      vim.opt.mouse = state and "a" or ""
-      mouse_enabled = state
-    end,
-  })
-  :map("<leader>uM")
+create_toggle("Mouse", function()
+  return mouse_enabled
+end, function(state)
+  vim.opt.mouse = state and "a" or ""
+  mouse_enabled = state
+end, "<leader>uM")
---
+++ lua/plugins/avante.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      mappings = {
+        ask = "<leader>Aa",
+        edit = "<leader>Ae",
+        refresh = "<leader>Ar",
+        focus = "<leader>af",
+        toggle = {
+          default = "<leader>At",
+          debug = "<leader>Ad",
+          hint = "<leader>Ah",
+          suggestion = "<leader>As",
+          repomap = "<leader>AR",
+        },
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+}
