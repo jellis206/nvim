fbcec82e0a86f2b39769145333762d5c3cd7c620
Jay Ellis <jgellis206+dev@gmail.com>
2025-10-01 12:32:41 -0600

    some minor updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,9 +6,6 @@ wk.add({
   { "<leader>'", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
   { "<leader>/", LazyVim.pick("live_grep", { root = true }), desc = "Grep" },
   { "<leader><space>", LazyVim.pick("files", { root = true }), desc = "Find Files" },
-  { "<leader>s", group = "search" },
-  { "<leader>sW", LazyVim.pick("grep_string", { word_match = "-w", root = false }), desc = "Word (cwd)" },
-  { "<leader>sw", LazyVim.pick("grep_string", { word_match = "-w", root = true }), desc = "Word (root dir)" },
 
   -- Buffer management
   { "<leader>bh", "<cmd>BufferLineCloseLeft<cr>", desc = "Close Left Buffers" },
@@ -35,14 +32,6 @@ wk.add({
   },
   { "<leader>pm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
   { "<leader>ut", "<cmd>Telescope undo<cr>", desc = "Undotree" },
-})
-
--- Visual mode mappings
-wk.add({
-  mode = { "v" },
-  { "<leader>s", group = "Search" },
-  { "<leader>sW", LazyVim.pick("grep_string", { root = false }), desc = "Selection (cwd)" },
-  { "<leader>sw", LazyVim.pick("grep_string", { root = true }), desc = "Selection (root dir)" },
 })
 
 -- Insert mode mappings
@@ -75,16 +64,16 @@ end, function(state)
 end, "<leader>ua")
 
 -- Copilot toggle
-create_toggle("Copilot", function()
-  return not require("copilot.client").is_disabled()
-end, function(state)
-  local copilot = require("copilot.command")
-  if state then
-    copilot.enable()
-  else
-    copilot.disable()
-  end
-end, "<leader>uc")
+-- create_toggle("Copilot", function()
+--   return not require("copilot.client").is_disabled()
+-- end, function(state)
+--   local copilot = require("copilot.command")
+--   if state then
+--     copilot.enable()
+--   else
+--     copilot.disable()
+--   end
+-- end, "<leader>uc")
 
 -- Git blame toggle
 local gb_enabled = true
--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -9,7 +9,7 @@ return {
         ask = "<leader>Aa",
         edit = "<leader>Ae",
         refresh = "<leader>Ar",
-        focus = "<leader>af",
+        focus = "<leader>Af",
         toggle = {
           default = "<leader>At",
           debug = "<leader>Ad",
@@ -21,15 +21,18 @@ return {
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
-      "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
+      -- "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      -- "stevearc/dressing.nvim", -- for input provider dressing
+      "folke/snacks.nvim", -- for input provider snacks
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      -- "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
@@ -42,6 +45,8 @@ return {
             drag_and_drop = {
               insert_mode = true,
             },
+            -- required for Windows users
+            use_absolute_path = true,
           },
         },
       },
