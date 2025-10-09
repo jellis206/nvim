fcf72c7f8937b974b53cd15dae83ecd05907df3f
Jay Ellis <jgellis206+dev@gmail.com>
2025-10-09 01:09:50 -0600

    conditionally disable img-clip

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,12 @@
-return {
-  -- { "echasnovski/mini.icons", enabled = false },
-}
+local plugins = {}
+
+local sysname = vim.loop.os_uname().sysname
+local is_linux = sysname == "Linux"
+local is_ssh = os.getenv("SSH_CONNECTION") ~= nil
+
+-- Disable img-clip.nvim when SSH'd into a Linux machine
+if is_linux and is_ssh then
+  table.insert(plugins, { "HakonHarnes/img-clip.nvim", enabled = false })
+end
+
+return plugins
