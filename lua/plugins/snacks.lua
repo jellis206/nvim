673b026d0d3b9c3eb7242a6c96438a5ec6376598
Jay Ellis <jgellis206+dev@gmail.com>
2025-10-23 17:16:31 -0600

    disable notifier

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -161,7 +161,7 @@ return {
       image = {},
       indent = { enabled = true },
       input = { enabled = true },
-      notifier = { enabled = true },
+      notifier = { enabled = false },
       scope = { enabled = true },
       scroll = { enabled = true },
       statuscolumn = { enabled = false }, -- we set this in options.lua
