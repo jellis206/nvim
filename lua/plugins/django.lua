a82138b16ddef01e21bb8db1ec50b149e28a161d
Jay Ellis <jgellis206+dev@gmail.com>
2025-11-12 16:47:49 -0700

    no formatting djangohtml

--- lua/plugins/django.lua
+++ lua/plugins/django.lua
@@ -24,17 +24,17 @@ return {
       },
     },
   },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        htmldjango = { "djlint" },
-      },
-      formatters = {
-        djlint = {
-          args = { "--reformat", "-" },
-        },
-      },
-    },
-  },
+  --   {
+  --     "stevearc/conform.nvim",
+  --     opts = {
+  --       formatters_by_ft = {
+  --         htmldjango = { "djlint" },
+  --       },
+  --       formatters = {
+  --         djlint = {
+  --           args = { "--reformat", "-" },
+  --         },
+  --       },
+  --     },
+  --   },
 }
