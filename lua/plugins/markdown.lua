2be74ceaeb12ddd16e483816d527850207000b75
Jay Ellis <jgellis206+dev@gmail.com>
2026-02-09 16:34:53 -0700

    add markdown plugin

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = function()
+            require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+            vim.fn["mkdp#util#install"]()
+        end,
+        keys = {
+            {
+                "<leader>pm",
+                ft = "markdown",
+                "<cmd>MarkdownPreviewToggle<cr>",
+                desc = "Markdown Preview",
+            },
+        },
+        config = function()
+            vim.cmd([[do FileType]])
+        end,
+    },
+}
