0844e00efc9fa606d9737b98272c9a9c612fc52e
Jay Ellis <jgellis206+dev@gmail.com>
2026-05-21 14:47:58 -0600

    update md previewer

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -10,11 +10,45 @@ return {
             {
                 "<leader>pm",
                 ft = "markdown",
-                "<cmd>MarkdownPreviewToggle<cr>",
+                function()
+                    local port_start = 8765
+                    local port_count = 8
+                    local function port_free(p)
+                        local sock = vim.uv.new_tcp()
+                        local ok = sock:bind("127.0.0.1", p)
+                        sock:close()
+                        return ok ~= nil
+                    end
+                    local chosen
+                    for p = port_start, port_start + port_count - 1 do
+                        if port_free(p) then
+                            chosen = p
+                            break
+                        end
+                    end
+                    if not chosen then
+                        vim.notify(
+                            ("All %d markdown-preview ports (%d-%d) are busy"):format(
+                                port_count,
+                                port_start,
+                                port_start + port_count - 1
+                            ),
+                            vim.log.levels.ERROR
+                        )
+                        return
+                    end
+                    vim.g.mkdp_port = tostring(chosen)
+                    vim.notify(("Markdown preview on http://localhost:%d"):format(chosen))
+                    vim.cmd("MarkdownPreviewToggle")
+                end,
                 desc = "Markdown Preview",
             },
         },
         config = function()
+            vim.g.mkdp_auto_start = 0
+            vim.g.mkdp_auto_close = 1
+            vim.g.mkdp_open_to_the_world = 0
+            vim.g.mkdp_browser = "true" -- /bin/true: no-op, headless VM has no browser
             vim.cmd([[do FileType]])
         end,
     },
