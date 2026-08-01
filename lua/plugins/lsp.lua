1a46cf92a0cb8348fa33b888d581a416f8ffd19c
Jay Ellis <jgellis206+dev@gmail.com>
2026-08-01 02:04:55 -0600

    update lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,57 +1,59 @@
 -- local util = require("lspconfig/util")
 
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "angular-language-server",
-        "typescript-language-server",
-        "clang-format",
-        "cpplint",
-        "cpptools",
-        "js-debug-adapter",
-        "debugpy",
-        "prettierd",
-        "reorder-python-imports",
-        "ruff",
-        "rust-analyzer",
-        "shfmt",
-        "rubocop",
-        "stylelint",
-        "misspell",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- make sure mason installs the server
-      servers = {
-        angularls = {
-          -- root_dir = util.root_pattern(".git", "nx.json", "angular.json", "tsconfig.json"),
-          root_dir = function()
-            return vim.loop.cwd()
-          end,
-          -- root_dir = function()
-          --   return vim.fs.find({ ".git", "nx.json", "package.json" }, {
-          --     upward = true,
-          --     stop = vim.uv.os_homedir(),
-          --   })
-          -- end,
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "angular-language-server",
+                "typescript-language-server",
+                "clang-format",
+                "cpplint",
+                "cpptools",
+                "js-debug-adapter",
+                "debugpy",
+                "prettierd",
+                "reorder-python-imports",
+                "ruff",
+                "rust-analyzer",
+                "shfmt",
+                "rubocop",
+                "stylelint",
+                "misspell",
+                "jq",
+                "json-repair",
+            },
         },
-        templ = {},
-        -- dartls = {},
-      },
     },
-  },
-  {
-    "nvim-flutter/flutter-tools.nvim",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            -- make sure mason installs the server
+            servers = {
+                angularls = {
+                    -- root_dir = util.root_pattern(".git", "nx.json", "angular.json", "tsconfig.json"),
+                    root_dir = function()
+                        return vim.loop.cwd()
+                    end,
+                    -- root_dir = function()
+                    --   return vim.fs.find({ ".git", "nx.json", "package.json" }, {
+                    --     upward = true,
+                    --     stop = vim.uv.os_homedir(),
+                    --   })
+                    -- end,
+                },
+                templ = {},
+                -- dartls = {},
+            },
+        },
     },
-    config = true,
-  },
+    {
+        "nvim-flutter/flutter-tools.nvim",
+        lazy = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "stevearc/dressing.nvim", -- optional for vim.ui.select
+        },
+        config = true,
+    },
 }
